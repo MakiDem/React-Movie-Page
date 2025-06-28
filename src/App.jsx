9e03399f6ca6923db4e81d6eff689e98a185fa45
Maki <mcjohndraycabral@gmail.com>
2025-06-28 18:55:51 +0800

    add: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Home from  "./pages/Home.jsx"
+import Favorites from "./pages/Favorites.jsx";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
+import {Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home/> }></Route>
+        <Route path='/favorites' element={ <Favorites />}></Route>
+      </Routes>
   );
 }
 
