8b701d7d9b9df788ff7df52c7b91c63fef992b8e
Maki <mcjohndraycabral@gmail.com>
2025-06-29 16:06:48 +0800

    add: css files of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from  "./pages/Home.jsx"
 import Favorites from "./pages/Favorites.jsx";
-import "./App.css";
+import "./css/App.css";
 import MovieCard from "./components/MovieCard";
 import {Routes, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar.jsx"
