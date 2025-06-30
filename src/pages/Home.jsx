a1938cdba2b7822d74b051797965a767d33b5cf4
Maki <mcjohndraycabral@gmail.com>
2025-06-30 17:13:22 +0800

    update: api principle of error and loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,31 @@
 import MovieCard from "../components/MovieCard.jsx"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import '../css/Home.css'
+import { getPopularMovies } from "../services/api.js"
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("")
-
-  const movies = [
-    {id: 1, title: "Train To Busan", release_date: 2024},
-    {id: 2, title: "28 Years Later", release_date: 2006},
-    {id: 3, title: "Titanic", release_date: 2021},
-    {id: 4, title: "Your Name", release_date: 2015},
-    {id: 5, title: "Infinity Train", release_date: 1987},
-  ]
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   const handleSearch = (e) => {
     e.preventDefault()
     alert("Searching movie...")
   }
+
+  useEffect(() => {
+    const loadPopularMovies = async() => {
+      try {
+        const popularMovies = await getPopularMovies()
+        setMovies(popularMovies)
+      } catch(err) {
+
+      } finally {
+
+      }
+    }
+  }, [movies])
 
   return (
     <div className = "home">
